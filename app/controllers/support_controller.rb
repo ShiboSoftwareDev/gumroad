d78293487723a27eec7ff2b12bb4a1dd8eb77091
Jono Mingard <reason.koan@gmail.com>
2025-08-09 22:32:01 +0100

    Fix

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -2,16 +2,12 @@ class SupportController < Sellers::BaseController
   include HelperWidget
 
   def index
-    timestamp = (Time.current.to_f * 1000).to_i
-    email_hash = OpenSSL::HMAC.hexdigest(
-      "sha256",
-      GlobalConfig.get("HELPER_WIDGET_SECRET"),
-      "#{current_seller.email}:#{timestamp}"
-    )
+    authorize :support
 
+    timestamp = (Time.current.to_f * 1000).to_i
     session = {
-      email: email,
-      emailHash: email_hash,
+      email: current_seller.email,
+      emailHash: helper_widget_email_hmac(timestamp),
       timestamp: timestamp,
     }
 
